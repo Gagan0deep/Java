61a846938f1f4599c9e7d9eb1a1462e1a0f3ff3a
Gagan0deep <36810167+Gagan0deep@users.noreply.github.com>
2018-06-16 23:33:58 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,21 @@
+package g;
+public class test{
+
+	public static int a=3,b=5;
+
+	public static void add(){
+		System.out.println(a+b);
+	}
+	public int mul(int a,int b){
+		return a*b;
+	}
+	int div(int a,int b){
+		return a/b;
+	}
+	 public test(){
+	System.out.println("Hello");
+	}
+	
+}
+	
+		
